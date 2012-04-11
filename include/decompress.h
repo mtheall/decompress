4a220ff3810d1584f6e9a1e3d9627759dd9addc2
mtheall <pigman46@gmail.com>
2012-04-11 13:54:34 -0500

    add lzss in C

---
+++ include/decompress.h
@@ -0,0 +1,9 @@
+#ifndef DECOMPRESS_H
+#define DECOMPRESS_H
+
+#include <nds.h>
+
+void lzssDecompress(const u8 *src, u8 *dst, int len);
+
+#endif /* DECOMPRESS_H */
+
--- source/main.c
+++ source/main.c
@@ -1,4 +1,42 @@
+#include <nds.h>
+#include <stdio.h>
+#include "decompress.h"
+#include "raw_img_bin.h"
+#include "lzss_img_bin.h"
+
+#define SIZE 49152
+static u8 buf[SIZE];
+
+void testLZSS();
+
 int main(int argc, char *argv[]) {
+  consoleDemoInit();
+
+  testLZSS();
+
+  while(1)
+    swiWaitForVBlank();
+
   return 0;
+}
+
+#define FAIL(routine)       printf("%-8s: failure\n", #routine)
+#define PASS(routine, time) printf("%-8s: %7d\n", #routine, time)
+
+void testLZSS() {
+  int timer;
+
+  memset(buf, 0, SIZE);
+  DC_FlushAll();
+  DC_InvalidateAll();
+
+  cpuStartTiming(0);
+  lzssDecompress(lzss_img_bin+4, buf, SIZE);
+  timer = cpuEndTiming();
+
+  if(memcmp(buf, raw_img_bin, SIZE))
+    FAIL(lzss_C);
+  else
+    PASS(lzss_C, timer);
 }
 
