8fb5bdeb117207e31d39c9c432f3647afb8c39c1
mtheall <pigman46@gmail.com>
2012-04-11 15:09:42 -0500

    rle_asm.s: add comments; interleave instructions between ldr/str where possible

--- source/rle_asm.s
+++ source/rle_asm.s
@@ -5,27 +5,29 @@
 .global rleDecompressASM
 .type rleDecompressASM, %function
 rleDecompressASM:
-  cmp    r2,  #0
-  bxle   lr
-  ldrb   r12, [r0], #1
-  tst    r12, #0x80
-  and    r12, r12, #0x7F
-  beq    .Luncompressed
-  add    r12, r12, #3
-  sub    r2,  r2,  r12
-  ldrb   r3, [r0], #1
+  cmp    r2,  #0             @ if(size <= 0)
+  bxle   lr                  @   return
+  ldrb   r12, [r0], #1       @ len = *in++
+  tst    r12, #0x80          @ if(len & 0x80 == 0)
+  and    r12, r12, #0x7F     @ <unconditionally> len &= 0x7F
+  beq    .Luncompressed      @   goto uncompressed
+
+  add    r12, r12, #3        @ len += 3
+  sub    r2,  r2,  r12       @ size -= len
+  ldrb   r3, [r0], #1        @ r3 = *in++
 .Lcopy_compressed:
-  strb   r3, [r1], #1
-  subs   r12, r12, #1
-  bne    .Lcopy_compressed
-  b      rleDecompressASM
+  subs   r12, r12, #1        @ --len
+  strb   r3, [r1], #1        @ *out++ = r3
+  bne    .Lcopy_compressed   @ if(len != 0) goto copy_compressed
+  b      rleDecompressASM    @ goto rleDecompressASM
+
 .Luncompressed:
-  add    r12, r12, #1
-  sub    r2,  r2,  r12
+  add    r12, r12, #1        @ len += 1
+  sub    r2,  r2,  r12       @ size -= len
 .Lcopy_uncompressed:
-  ldrb   r3, [r0], #1
-  strb   r3, [r1], #1
-  subs   r12, r12, #1
-  bne    .Lcopy_uncompressed
-  b      rleDecompressASM
+  ldrb   r3, [r0], #1        @ r3 = *in++
+  subs   r12, r12, #1        @ --len
+  strb   r3, [r1], #1        @ *out++ = r3
+  bne    .Lcopy_uncompressed @ if(len != 0) goto copy_uncompressed
+  b      rleDecompressASM    @ goto rleDecompressASM
 
