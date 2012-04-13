a8a0f3f29cd29267a4e59464822439c0d4021df7
mtheall <pigman46@gmail.com>
2012-04-13 17:09:32 -0500

    lz11_asm.s: replace counter with rotating mask

--- source/lz11_asm.s
+++ source/lz11_asm.s
@@ -6,20 +6,16 @@
 .type lz11DecompressASM, %function
 lz11DecompressASM:
   push   {r4-r6}
-.Lmain_loop:
-  ldrb   r12, [r0], #1       @ flags = *in++
-  mov    r3, #0              @ i = 0
+  ldr    r3, =(0x01010101)
 
-.Lsub_loop:
+.Lloop:
   cmp    r2, #0              @ if(size <= 0)
   pople  {r4-r6}             @   pop stack
   bxle   lr                  @   return
 
-  cmp    r3, #8              @ if(i == 8)
-  beq    .Lmain_loop         @   goto main_loop
-
-  tst    r12, #0x80          @ if(flags & 0x80 != 0x80)
-  lsl    r12, r12, #1        @ (unconditionally) flags <<= 1
+  rors   r3, r3, #1          @ r3 = (r3<<31) | (r3>>1)
+  ldrcsb r12, [r0], #1       @ if(r3 & (1<<31)) flags = *in++
+  tst    r12, r3             @ if(flags & r3 == 0)
   beq    .Lcopy_uncompressed @   goto copy_uncompressed
 
   ldrb   r4, [r0], #1        @ r4 = *in++
@@ -56,11 +52,10 @@ lz11DecompressASM:
   orr    r4, r4, r5, lsl #8  @ disp = r4 | (disp<<8) note: disp changes to r4
   add    r4, r4, #1          @ disp++
   sub    r2, r2, r6          @ size -= len
-  add    r3, r3, #1          @ i++
 
 .Lcopy_compressed:
   subs   r6, r6, #1          @ if(len-- == 1)
-  blt    .Lsub_loop          @   goto sub_loop
+  blt    .Lloop              @   goto loop
   ldrb   r5, [r1, -r4]       @ r5 = *(out - disp)
   strb   r5, [r1], #1        @ *out++ = r5
   b      .Lcopy_compressed   @ goto copy_compressed
@@ -70,5 +65,5 @@ lz11DecompressASM:
   strb   r4, [r1], #1        @ *out++ = r4
   add    r3, r3, #1          @ i++
   sub    r2, r2, #1          @ size--
-  b      .Lsub_loop          @ goto sub_loop
+  b      .Lloop              @ goto loop
 
